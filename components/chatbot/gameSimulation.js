fb4ee2382dbf1c918ea316312b7045901a343999
Junfei <Junfei.Yu.TH@dartmouth.edu>
2018-05-24 01:01:03 -0400

    Output questins based upon certain game list for post game evaluation

---
+++ components/chatbot/gameSimulation.js
@@ -0,0 +1,23 @@
+export default {
+  date: '12-17-43',
+  time: '',
+  duration: '2 hrs',
+  lat: 50,
+  long: 50,
+  playersSignedUp: ['player 1', 'player 2', 'player 3', 'player 4', 'player 5'],
+  postGameEvaluation: [{
+    'player 2': {
+      'player 1': 'Dislike', 'player 3': 'Dislike', 'player 4': 'Like', 'player 5': 'Like',
+    },
+  },
+  {
+    'player 3': {
+      'player 1': 'Dislike', 'player 2': 'Dislike', 'player 4': 'Like', 'player 5': 'Like',
+    },
+  },
+  ],
+  max_players: 10,
+  author: 'player 1',
+  level: 'Intense',
+
+};
